619e5743f91f4ef0d66e60b2db6b5cd8468a8196
Dawood Multhanwala <dawood.multhan@gmail.com>
2019-05-07 10:15:09 +0100

    adding our index page

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path =require("path");
+const express=require("express");
+const PORT=process.env.PORT || 3000;
+const publicPath=path.join(__dirname,"..","/public");
+const app= express();
+app.use(express.static(publicPath));
+
+app.listen(PORT,()=>{
+    console.log(`Application is started on port ${PORT}`);
+})
