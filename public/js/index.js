f07940a197876c01a4e003c3ab4fa4e9f95b430f
Dawood Multhanwala <dawood.multhan@gmail.com>
2019-05-10 17:41:57 +0100

    adding message route

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket =io();
+socket.on('connect',function (){
+    console.log("connected to the browser");
+    socket.emit("createmessage",{
+        to:"jen shaw",
+        text:"hello jen how are you"
+    })
+});
+
+socket.on('disconnect',function(){
+    console.log("disconnected from server");
+});
+
+socket.on("newMessage",function(newmsg){
+    console.log("message from the server is" +newmsg);
+});
--- server/server.js
+++ server/server.js
@@ -11,8 +11,16 @@ let io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log("new user connected");
+socket.emit('newMessage',{
+    to:"shauna morkel",
+    text:"hello shauna where to meet",
+    createdAt:new Date()
+});
 socket.on('disconnect',()=>{
     console.log("disconnected from the client");
+})
+socket.on("createMessage",(createmsg)=>{
+    console.log("you have to create the msg"+createmsg);
 })
 });
 
