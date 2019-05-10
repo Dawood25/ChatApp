bd846e1c40ede9fa4b10cb76d778d4d5b2deabd3
Dawood Multhanwala <dawood.multhan@gmail.com>
2019-05-10 13:34:24 +0100

    added connection with io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const path =require("path");
 const express=require("express");
+const http=require("http");
+const socketIO=require("socket.io");
 const PORT=process.env.PORT || 3000;
 const publicPath=path.join(__dirname,"..","/public");
 const app= express();
-app.use(express.static(publicPath));
+let server=http.createServer(app);
+let io=socketIO(server);
 
-app.listen(PORT,()=>{
+app.use(express.static(publicPath));
+io.on('connection',(socket)=>{
+console.log("new user connected");
+socket.on('disconnect',()=>{
+    console.log("disconnected from the client");
+})
+});
+
+server.listen(PORT,()=>{
     console.log(`Application is started on port ${PORT}`);
 })
